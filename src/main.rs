d221195a53e13008d726c39d65304f18d2942528
philsippl <philsippl@gmail.com>
2024-12-15 18:45:53 +0000

    init

---
+++ src/main.rs
@@ -0,0 +1,63 @@
+use std::sync::Arc;
+
+use aws_config;
+use aws_sdk_s3::config::BehaviorVersion;
+use aws_sdk_s3::Client;
+use memmap2::MmapMut;
+use s3::{fetch_to_mem, list_objects};
+use tokio::time::Instant;
+
+mod s3;
+
+const ITERATIONS: usize = 1;
+const N_OBJECTS: usize = 10;
+
+#[tokio::main]
+async fn main() -> eyre::Result<()> {
+    tracing_subscriber::fmt::init();
+    let config = aws_config::defaults(BehaviorVersion::latest()).load().await;
+
+    let objects = list_objects(
+        Arc::new(Client::new(&config)),
+        "s3-bench-test-bucket-ps".to_string(),
+        "test_file",
+    )
+    .await?[..N_OBJECTS]
+        .to_vec();
+
+    // allocate memory for the objects
+    const MEM_BUFFER_SIZE: usize = N_OBJECTS * (1 << 30);
+    let mut buffer = MmapMut::map_anon(MEM_BUFFER_SIZE).unwrap();
+
+    tracing::info!(
+        "[+] Allocated {}GB of memory",
+        MEM_BUFFER_SIZE as f64 / (1 << 30) as f64
+    );
+
+    // Write zeroes to the memory to avoid page faults
+    unsafe {
+        std::ptr::write_bytes(buffer.as_mut_ptr(), 0, MEM_BUFFER_SIZE);
+    }
+
+    tracing::info!("[+] Memory prepared. Starting benchmark...");
+
+    let now = Instant::now();
+    for _ in 0..ITERATIONS {
+        fetch_to_mem(
+            Arc::new(Client::new(&config)),
+            objects.clone(),
+            "s3-bench-test-bucket-ps".to_string(),
+            buffer.as_mut_ptr(),
+        )
+        .await?;
+    }
+    let elapsed = now.elapsed();
+
+    println!("elapsed: {:?}", elapsed);
+    let gbps =
+        (objects.len() * 1024 * 1024 * 1024 * 8) as f64 / elapsed.as_secs_f64() / 1_000_000_000.0
+            * ITERATIONS as f64;
+    println!("[*] Throughput: {:.2} Gbps", gbps);
+
+    Ok(())
+}
---
+++ src/s3.rs
@@ -0,0 +1,95 @@
+use std::sync::Arc;
+
+use aws_sdk_s3::Client;
+use tokio::{io::AsyncReadExt, task};
+
+const CHUNK_SIZE: usize = 1 << 30;
+const PART_SIZE: usize = 50 << 20;
+
+pub async fn list_objects(
+    client: Arc<Client>,
+    bucket: String,
+    prefix: &str,
+) -> eyre::Result<Vec<String>> {
+    let mut objects = Vec::new();
+    let mut continuation_token = None;
+
+    loop {
+        let mut request = client.list_objects_v2().bucket(&bucket).prefix(prefix);
+
+        if let Some(token) = continuation_token {
+            request = request.continuation_token(token);
+        }
+
+        let response = request.send().await?;
+
+        objects.extend(
+            response
+                .contents()
+                .iter()
+                .filter_map(|obj| obj.key().map(String::from)),
+        );
+
+        match response.next_continuation_token() {
+            Some(token) => continuation_token = Some(token.to_string()),
+            None => break,
+        }
+    }
+
+    Ok(objects)
+}
+
+pub async fn fetch_to_mem(
+    client: Arc<Client>,
+    chunks: Vec<String>,
+    bucket: String,
+    mem_ptr: *mut u8,
+) -> eyre::Result<()> {
+    let mut handles: Vec<task::JoinHandle<Result<(), eyre::Error>>> =
+        Vec::with_capacity(chunks.len());
+    for (chunk_idx, chunk) in chunks.into_iter().enumerate() {
+        for part_idx in 0..(CHUNK_SIZE / PART_SIZE) {
+            let client = client.clone();
+            let bucket = bucket.clone();
+            let chunk = chunk.clone();
+            let slice = unsafe {
+                std::slice::from_raw_parts_mut(
+                    mem_ptr.add(chunk_idx * CHUNK_SIZE + part_idx * PART_SIZE),
+                    PART_SIZE,
+                )
+            };
+            handles.push(task::spawn(async move {
+                let mut result = client
+                    .get_object()
+                    .bucket(&bucket)
+                    .key(&chunk)
+                    .range(format!(
+                        "bytes={}-{}",
+                        part_idx * PART_SIZE,
+                        (part_idx + 1) * PART_SIZE - 1
+                    ))
+                    .send()
+                    .await?
+                    .body
+                    .into_async_read();
+
+                let mut bytes_read = 0;
+                while bytes_read < CHUNK_SIZE {
+                    let n = result.read(&mut slice[bytes_read..]).await?;
+                    if n == 0 {
+                        break;
+                    }
+                    bytes_read += n;
+                }
+
+                Ok(())
+            }));
+        }
+    }
+
+    for handle in handles {
+        handle.await??;
+    }
+
+    Ok(())
+}
